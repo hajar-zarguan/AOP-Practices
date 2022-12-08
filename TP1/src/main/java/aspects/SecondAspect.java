96e9dd9258c6d4dae9bdb7f1176332cc5a353c70
hajar-zarguan <hajar.zarguan.0610@gmail.com>
2022-12-08 22:42:34 +0100

    Class syntax around Proceed

--- TP1/src/main/java/aspects/SecondAspect.java
+++ TP1/src/main/java/aspects/SecondAspect.java
@@ -1,9 +1,7 @@
 package aspects;
 
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 
 @Aspect
 public class SecondAspect {
@@ -11,6 +9,7 @@ public class SecondAspect {
     public  void pc1(){
 
   }
+  /*
   @Before("pc1()")
   public  void  beforeMain(){
       System.out.println("********************************************");
@@ -23,5 +22,18 @@ public class SecondAspect {
         System.out.println("After main from AspectJ with class syntax");
         System.out.println("*********************************************");
     }
+*/
+    @Around("pc1()")
+    public  void  AroundMain(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        System.out.println("********************************************");
+        System.out.println("before main from AspectJ with class syntax");
+        System.out.println("*********************************************");
 
+        proceedingJoinPoint.proceed();
+
+        System.out.println("********************************************");
+        System.out.println("After main from AspectJ with class syntax");
+        System.out.println("*********************************************");
+
+    }
 }
