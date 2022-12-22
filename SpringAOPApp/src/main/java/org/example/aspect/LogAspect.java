fd1d644f37ad116e0ac44c8e91569ec22161f0ca
hajar-zarguan <hajar.zarguan.0610@gmail.com>
2022-12-22 16:17:34 +0100

    Proxy

--- SpringAOPApp/src/main/java/org/example/aspect/LogAspect.java
+++ SpringAOPApp/src/main/java/org/example/aspect/LogAspect.java
@@ -7,18 +7,21 @@ import org.aspectj.lang.annotation.Before;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Component;
 
+import java.util.logging.Logger;
+
 @Component
 @EnableAspectJAutoProxy
 @Aspect
 public class LogAspect {
+    Logger logger = Logger.getLogger(LogAspect.class.getName());
     @Around("execution(* org.example.service..*(..))")
     public Object log (ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         long t1 = System.currentTimeMillis();
-        System.out.println("From Logging Aspects.....Before "+ proceedingJoinPoint.getSignature());
+        logger.info("From Logging Aspects.....Before "+ proceedingJoinPoint.getSignature());
         Object result = proceedingJoinPoint.proceed();
-        System.out.println("From Logging Aspects.....Before "+ proceedingJoinPoint.getSignature());
+        logger.info("From Logging Aspects.....Before "+ proceedingJoinPoint.getSignature());
         long t2 = System.currentTimeMillis();
-        System.out.println("Duration :"+(t2-t1));
+        logger.info("Duration :"+(t2-t1));
         return result;
 
     }
