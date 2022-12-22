32dd7f38dd1cedb1033ee25793f1937c97968822
hajar-zarguan <hajar.zarguan.0610@gmail.com>
2022-12-22 16:12:43 +0100

    Around

--- SpringAOPApp/src/main/java/org/example/aspect/LogAspect.java
+++ SpringAOPApp/src/main/java/org/example/aspect/LogAspect.java
@@ -1,5 +1,7 @@
 package org.example.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -9,9 +11,16 @@ import org.springframework.stereotype.Component;
 @EnableAspectJAutoProxy
 @Aspect
 public class LogAspect {
-    @Before("execution(public  void  process())")
-    public void log (){
-        System.out.println("From Logging Aspects.....Before");
+    @Around("execution(* org.example.service..*(..))")
+    public Object log (ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        long t1 = System.currentTimeMillis();
+        System.out.println("From Logging Aspects.....Before "+ proceedingJoinPoint.getSignature());
+        Object result = proceedingJoinPoint.proceed();
+        System.out.println("From Logging Aspects.....Before "+ proceedingJoinPoint.getSignature());
+        long t2 = System.currentTimeMillis();
+        System.out.println("Duration :"+(t2-t1));
+        return result;
+
     }
 
 }
